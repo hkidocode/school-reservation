9410675cb8fa15f47b5c639cb8051f2dc86e4448
hkidocode <57941226+hkidocode@users.noreply.github.com>
2021-03-16 21:34:28 +0100

    Revert "make some testing"
    This reverts commit cc8a41491ba5452d777d4b079a6fa5b1e3da1ca4.

--- src/main/java/ma/youcode/config/WebApplicationContextConfig.java
+++ src/main/java/ma/youcode/config/WebApplicationContextConfig.java
@@ -24,12 +24,8 @@ public class WebApplicationContextConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/files/**")
-                .addResourceLocations("/WEB-INF/files/");
-
+        registry.addResourceHandler("/files/**").addResourceLocations("/files/");
     }
 
 
 }
-
