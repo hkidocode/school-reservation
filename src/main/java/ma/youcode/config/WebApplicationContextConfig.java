cc8a41491ba5452d777d4b079a6fa5b1e3da1ca4
hkidocode <57941226+hkidocode@users.noreply.github.com>
2021-03-16 21:34:09 +0100

    make some testing

--- src/main/java/ma/youcode/config/WebApplicationContextConfig.java
+++ src/main/java/ma/youcode/config/WebApplicationContextConfig.java
@@ -24,8 +24,12 @@ public class WebApplicationContextConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/files/**").addResourceLocations("/files/");
+        registry
+                .addResourceHandler("/files/**")
+                .addResourceLocations("/WEB-INF/files/");
+
     }
 
 
 }
+
